c26c4cca58940ca4149d1bc3362869132879e95c
Warren Niu <warrenniu@gmail.com>
2020-09-04 17:49:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,6 +3,6 @@ def square_array(array)
   new_array = []
   length = array.length
   length.times do |index|
-    new_array.push(square(array[index]))
+    new_array.push(array[index] ** 2)
   end
 end
